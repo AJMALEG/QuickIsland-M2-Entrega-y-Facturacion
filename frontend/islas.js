9dba580c0b584d084c11774fdd9686a40f41836b
AJMALEG <newluxer2000@gmail.com>
2026-04-28 22:14:20 -0600

    Update islas.js

--- frontend/islas.js
+++ frontend/islas.js
@@ -1,1 +1,9 @@
 
+// Js/islas.js
+function irAIsla(idIsla) {
+    // Guardamos qué isla eligió el cliente para usarla en la siguiente página
+    localStorage.setItem('isla_seleccionada_id', idIsla);
+    
+    // Lo mandamos a la página donde verá los productos (Crea este .html si no lo tienes)
+    window.location.href = 'productos_isla.html';
+}
